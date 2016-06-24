d25013a1476706a624af5c67ca66953994cebcef
lufei2015 <460617421@qq.com>
2016-06-24 15:51:24 +0800

    Create appsConfig.php

---
+++ apps/config/dev/appsConfig.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 
+ * User: 30feifei@gamil.com
+ * Date: 2016/6/22
+ * Time: 17:18
+ */
+
+return array(
+    'app_path'=>'apps',
+    'time_zone'=>'Asia/Shanghai',
+    'mongoDb'=>array(
+        'default'=>array(
+            'server',
+            'port',
+            'dbname'
+        )
+    ),
+    'RedisDb'=>array(
+        'default'=>array(
+            'server',
+            'port',
+            'db'
+        )
+    ),
+);
